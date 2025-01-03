dd1056e88ace51e14664a68b259319a5640119d4
Chris Obdam <chris.obdam@bettyblocks.com>
2025-01-03 21:21:27 +0100

    always call action-b for now

--- action-runner/src/lib.rs
+++ action-runner/src/lib.rs
@@ -10,8 +10,10 @@ impl Guest for ActionRunner {
             "runtime-cloud",
             "action",
         );
+        // yourinterface.set_actor_id("bb_runtime_cloud-action_example_b".to_string());
+        // yourinterface.set_operation("execute".to_string());
 
-        wasmcloud::bus::lattice::set_link_name("action-example-b", vec![yourinterface]);
+        // wasmcloud::bus::lattice::set_link_name("action-example-b", vec![yourinterface]);
 
         // Calls over link foo to perform a keyvalue operation
         let x = bettyblocks::runtime_cloud::action::execute();
