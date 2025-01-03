51cb33d6b6016dca9ab6b143bcf18d6a62082a68
Chris Obdam <chris.obdam@bettyblocks.com>
2025-01-03 21:20:44 +0100

    add data-api-gql-proxy

--- action-example-b/src/lib.rs
+++ action-example-b/src/lib.rs
@@ -1,11 +1,13 @@
 wit_bindgen::generate!({ generate_all });
+use crate::bettyblocks::runtime_cloud::system_info::Kind;
 use exports::bettyblocks::runtime_cloud::action::Guest;
 
 struct Action;
 
 impl Guest for Action {
     fn execute() -> String {
-        let str = format!("action b");
+        let s = crate::bettyblocks::runtime_cloud::system_info::request_info(Kind::Os);
+        let str = format!("action b {}", s);
         str
     }
 }
---
+++ data-api-gql-proxy/README.md
@@ -0,0 +1,3 @@
+# DataAPI GraphQL Proxy WasmCloud provider
+
+This provider functions as a proxy between the Betty Blocks Data API and the Betty Blocks WasmCloud Runtime Cloud
